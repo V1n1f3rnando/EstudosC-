9f078af5ec194b869fadd754fec39c988ccebe27
Vinicius Fernando <31803742+V1n1f3rnando@users.noreply.github.com>
2018-07-21 08:47:51 -0300

    Continuação dos estudos de OO

---
+++ Projeto02/EstudosOOPt1/Entidades/Funcionario.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EstudosOOPt1.Entidades
+{
+    public class Funcionario
+    {
+        //Encapsulamento Inplícito 
+        public int IdFuncionario { get; set; }
+        public string Nome { get; set; }
+        public decimal Salario { get; set; }
+        public DateTime DataAdmissao { get; set; }
+
+        //Construtor
+        public Funcionario()
+        {
+
+        }
+
+        //Sobrecarregando o Construtor
+        public Funcionario(int idFuncionario, string nome, decimal salario, DateTime dataAdmissao)
+        {
+            IdFuncionario = idFuncionario;
+            Nome = nome;
+            Salario = salario;
+            DataAdmissao = dataAdmissao;
+        }
+
+        //Sobrescrevendo o método ToString da classe object
+        public override string ToString()
+        {
+            return $"\n ID:{IdFuncionario}\n Nome:{Nome}\n Salário: R${Salario}\n DataAdmissão: {DataAdmissao}";
+        }
+    }
+}
