f5bad06a14528db1b34a0db20373077626d42f4f
Vinicius Fernando <31803742+V1n1f3rnando@users.noreply.github.com>
2018-07-21 08:04:25 -0300

    Criação do método ExportarParaCsv

--- Projeto01/EstudosOOpt2/Armazenamento/ClienteArquivo.cs
+++ Projeto01/EstudosOOpt2/Armazenamento/ClienteArquivo.cs
@@ -10,8 +10,36 @@ namespace EstudosOOpt2.Armazenamento
 {
     public class ClienteArquivo
     {
-        //Passando como parâmetro o diretório do arquivo, e usando a expressão booleana true para acrescentar dados ao arquivo.
-        StreamWriter sw = new StreamWriter("c:\\temp\\cliente.txt", true); 
+        
+
+        //Método para exportar os dados do cliente para txt.
+        public void ExportarParaTxt(Cliente c)
+        {
+            //Passando como parâmetro o diretório do arquivo, e usando a expressão booleana true para acrescentar dados ao arquivo.
+            StreamWriter sw =
+                new StreamWriter("c:\\temp\\cliente.txt", true);
+
+            sw.WriteLine("ID: " + c.IdCliente);
+            sw.WriteLine("Nome: " + c.Nome);
+            sw.WriteLine("Email: " + c.Email);
+            sw.WriteLine("...");
+
+            //Fechar arquivo...
+            sw.Close();
+        }
+
+        //Método para exportar os dados do cliente para CSV.
+        public void ExportarParaCsv(Cliente c)
+        {
+            StreamWriter sw = new StreamWriter("c:\\temp\\cliente.csv", true);
+
+            //Gravando dados utilizando delimitador ';'
+            sw.WriteLine("{0};{1};{2}", c.IdCliente, c.Nome, c.Email);
+
+            // fechando arquivo
+            sw.Close();
+
+        }
 
 
     }
