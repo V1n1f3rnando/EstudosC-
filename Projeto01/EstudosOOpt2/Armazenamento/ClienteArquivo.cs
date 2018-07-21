866ee276b1bc03ffe6a7b2624c6aa8f72a018674
Vinicius Fernando <31803742+V1n1f3rnando@users.noreply.github.com>
2018-07-21 07:54:40 -0300

    Importanto a biblioteca System.IO e fazendo uso da classe StreamWriter.

---
+++ Projeto01/EstudosOOpt2/Armazenamento/ClienteArquivo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EstudosOOpt2.Entidades;// Importando o namespace..
+using System.IO; //Importando a classe do namespace System.IO para criar um arquivo de gravação
+
+namespace EstudosOOpt2.Armazenamento
+{
+    public class ClienteArquivo
+    {
+        //Passando como parâmetro o diretório do arquivo, e usando a expressão booleana true para acrescentar dados ao arquivo.
+        StreamWriter sw = new StreamWriter("c:\\temp\\cliente.txt", true); 
+
+
+    }
+}
---
+++ Projeto01/EstudosOOpt2/Entidades/Cliente.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EstudosOOpt2.Entidades
+{
+    public class Cliente
+    {
+        //Atributos privates, acesso somente dentro da classe.
+        private int idCliente;
+        private string nome;
+        private string email;
+
+        //Métodos..
+        //encapsulamento (set [entrada], get [saída])
+        public int IdCliente
+        {
+            set { idCliente = value; } //Entrada
+            get { return idCliente; } //saída
+        }
+
+        public string Nome
+        {
+            set { nome = value; }
+            get { return nome; }
+        }
+
+        public string Email
+        {
+            set { email = value; }
+            get { return email; }
+        }
+
+    }
+}
