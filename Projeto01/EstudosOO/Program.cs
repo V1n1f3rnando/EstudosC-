85d9037e9b099667660e088a5ec83ffd8b74d2d2
Vinicius Fernando <31803742+V1n1f3rnando@users.noreply.github.com>
2018-07-21 06:59:28 -0300

    Primeiros passos.

---
+++ Projeto01/EstudosOO/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EstudosOO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Estudo01 - C# ");
+
+            Console.Write("Informe seu nome: ");
+            string nome = Console.ReadLine();
+
+            Console.WriteLine("Seja bem vindo:"+nome);
+
+            Console.ReadKey();
+        }
+    }
+}
