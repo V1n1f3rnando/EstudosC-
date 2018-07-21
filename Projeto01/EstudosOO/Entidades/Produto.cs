239eac6a36e63dd2d46f44d1d00eccc59731274f
Vinicius Fernando <31803742+V1n1f3rnando@users.noreply.github.com>
2018-07-21 07:07:54 -0300

    Criação da entidade Produto

---
+++ Projeto01/EstudosOO/Entidades/Produto.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EstudosOO.Entidades
+{
+    public class Produto
+    {
+        public int IdProduto { get; set; }
+        public string Nome { get; set; }
+        public decimal Preco { get; set; }
+
+        //Construtor 
+        public Produto()
+        {
+
+        }
+
+        //Sobrecarregando o construtor 
+        public Produto(int idProduto, string nome, decimal preco)
+        {
+            IdProduto = idProduto;
+            Nome = nome;
+            Preco = preco;
+        }
+
+        //Sobrescrevendo o Método ToString da classe object 
+        public override string ToString()
+        {
+            return $"ID{IdProduto}\n Produto:{Nome}\n Preço: R${Preco}";
+        }
+    }
+}
