5c3443546e16b518aa1ae730ed15f35ab96d5a80
Vinicius Fernando <31803742+V1n1f3rnando@users.noreply.github.com>
2018-07-21 10:30:28 -0300

    Criação do namespace Controles e do método de exportação de dados para xml

---
+++ Projeto02/EstudosOOPt2/Controles/ClienteControle.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml; //Importando
+using EstudosOOPt2.Entidades; //Importando
+
+namespace EstudosOOPt2.Controles
+{
+    public class ClienteControle
+    {
+        //Método para exportar dados para XML
+        public void ExportarDadosXml(Cliente c)
+        {
+            //criando o arquivo XML
+            XmlWriter xml = XmlWriter.Create("c:\\temp\\cliente.xml");
+            xml.WriteStartDocument();//Escrever o cabeçalho do XML
+
+            xml.WriteStartElement("Cliente"); // <Cliente>
+
+            xml.WriteStartElement("idCliente", c.IdCliente.ToString());
+            xml.WriteStartElement("nome", c.Nome);
+            xml.WriteStartElement("email", c.Email);
+
+            xml.WriteStartElement("Endereco");
+
+            xml.WriteStartElement("idEndereco", c.Endereco.IdEndereco.ToString());
+            xml.WriteStartElement("logradouro", c.Endereco.Logradouro);
+            xml.WriteStartElement("cidade", c.Endereco.Cidade);
+            xml.WriteStartElement("estado", c.Endereco.Estado);
+            xml.WriteStartElement("cep", c.Endereco.Cep);
+
+            xml.WriteEndElement(); // </endereco>
+            xml.WriteEndElement(); // </cliente>
+
+            //fechando o arquivo
+            xml.Close();
+        }
+    }
+}
--- Projeto02/EstudosOOPt2/Program.cs
+++ Projeto02/EstudosOOPt2/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using EstudosOOPt2.Entidades; //Importando
 
 namespace EstudosOOPt2
 {
@@ -10,7 +11,19 @@ namespace EstudosOOPt2
     {
         static void Main(string[] args)
         {
+            Cliente c = new Cliente(); // inicializando
+            c.Endereco = new Endereco();
 
+            c.IdCliente = 1;
+            c.Nome = "Vinicius";
+            c.Email = "bgvinicius@exemplo.com";
+            c.Endereco.IdEndereco = 1;
+            c.Endereco.Logradouro = "Rua Carla 12, Roncador";
+            c.Endereco.Cidade = "Queimados";
+            c.Endereco.Estado = "RJ";
+            c.Endereco.Cep = "26381-754";
+
+            Console.ReadKey();
         }
     }
 }
