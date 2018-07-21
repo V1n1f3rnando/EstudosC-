7952aaa2a6383250c444e2583008f98f2af8bc67
Vinicius Fernando <31803742+V1n1f3rnando@users.noreply.github.com>
2018-07-21 08:16:10 -0300

    Fim do projeto e continuidade dos estudos.

--- Projeto01/EstudosOOpt2/Program.cs
+++ Projeto01/EstudosOOpt2/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using EstudosOOpt2.Entidades; //Importando o namespace Entidades
+using EstudosOOpt2.Armazenamento; //Importando o namespace que contém os métodos de gravação
 
 namespace EstudosOOpt2
 {
@@ -10,7 +12,39 @@ namespace EstudosOOpt2
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("\n --- CONTROLE DE CLIENTES ---");
+            
+            // Instânciando a entidade Cliente
+            var c = new Cliente();
 
+            Console.Write("\n Informe o ID: ");
+            c.IdCliente = int.Parse(Console.ReadLine());
+
+            Console.Write("\n Informe o nome: ");
+            c.Nome = Console.ReadLine();
+
+            Console.Write("\n Informe o email: ");
+            c.Email = Console.ReadLine();
+
+            //Tratamento de exceção 
+            try
+            {
+                //Instânciando a entidade ClienteArquivo e fazendo o uso dos métodos de exportação
+                var arquivo = new ClienteArquivo();
+
+                arquivo.ExportarParaTxt(c);
+                arquivo.ExportarParaCsv(c);
+
+                Console.WriteLine("\n Dados gravados com sucesso !! ");
+                
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine("Falha ao gravar o arquivo !! erro:"+ex.ToString());
+            }
+
+            Console.ReadKey();
         }
     }
 }
