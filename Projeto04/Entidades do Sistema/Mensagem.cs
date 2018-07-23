eef8c3464dfe8aaf568604b850eadcf5cecb282a
Vinicius Fernando <31803742+V1n1f3rnando@users.noreply.github.com>
2018-07-23 19:43:06 -0300

    Inicio do projeto.
    Criando classe para enviar e-mail.

---
+++ Projeto04/Entidades do Sistema/Mensagem.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projeto.Entidades
+{
+    public class Mensagem
+    {
+        public Guid IdMensagem { get; set; }
+        public string EmailDestino { get; set; }
+        public string Assunto { get; set; }
+        public string Conteudo { get; set; }
+
+        public Mensagem()
+        {
+
+        }
+
+        public Mensagem(Guid idMensagem, string emailDestino, string assunto, string conteudo)
+        {
+            IdMensagem = idMensagem;
+            EmailDestino = emailDestino;
+            Assunto = assunto;
+            Conteudo = conteudo;
+        }
+
+        public override string ToString()
+        {
+            return string.Format("ID:{0}, Para:{1}, Assunto:{2}, Conteúdo{3}", IdMensagem, EmailDestino, Assunto, Conteudo);
+        }
+    }
+}
---
+++ Projeto04/Entidades do Sistema/MensagemInput.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades_do_Sistema
+{
+    public class MensagemInput
+    {
+        public static string LerEmailDestino()
+        {
+            Console.WriteLine("Informe o email de destino: ");
+            return Console.ReadLine();
+        }
+
+        public static string LerAssunto()
+        {
+            Console.WriteLine("Informe o assunto da mensagem: ");
+            return Console.ReadLine();
+        }
+
+        public static string LerConteudo()
+        {
+            Console.WriteLine("Informe o conteudo da mensagem: ");
+            return Console.ReadLine();
+        }
+
+    }
+}
