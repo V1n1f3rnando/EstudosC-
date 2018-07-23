3452010f30fa711bcd9e02faf9aa65b037be82c4
Vinicius Fernando <31803742+V1n1f3rnando@users.noreply.github.com>
2018-07-23 12:36:16 -0300

    Finalização do cadastro de pessoas.

---
+++ Projeto03/EstudosPOOPt1/Controles/PessoaControle.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EstudosPOOPt1.Entidades;
+using EstudosPOOPt1.Repositorios;
+
+namespace EstudosPOOPt1.Controles
+{
+    public class PessoaControle
+    {
+       public void CadastrarPessoa()
+        {
+            Console.WriteLine("\n - CADASTRO DE PESSOA \n ");
+
+            try
+            {
+                var p = new Pessoa();
+
+                Console.Write("Informe o nome: ");
+                p.Nome = Console.ReadLine();
+
+                Console.WriteLine("Informe a data de Nasc:");
+                p.DataNasc = DateTime.Parse(Console.ReadLine());
+
+                var rep = new PessoaRepositorio();
+                rep.Cadastrar(p);
+
+                Console.WriteLine("\n Pessoa cadastrada com sucesso !");
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine("Erro: "+ex.Message);
+            }
+        }
+        
+
+
+    }
+}
--- Projeto03/EstudosPOOPt1/Repositorios/PessoaRepositorio.cs
+++ Projeto03/EstudosPOOPt1/Repositorios/PessoaRepositorio.cs
@@ -3,12 +3,30 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using EstudosPOOPt1.Entidades;
+using System.Data.SqlClient;
 
 
 namespace EstudosPOOPt1.Repositorios
 {
-    public class PessoaRepositorio
+    public class PessoaRepositorio : Conexao
     {
-        
+        public void Cadastrar(Pessoa p)
+        {
+            AbrirConexao();
+
+            //Escrevendo o comando em sql
+            string query = "insert into Pessoa (nome, dataNascimento)" +
+                " values (@nome, @dataNascimento) ";
+
+            //executando o comando 
+            cmd = new SqlCommand(query, con);
+            cmd.Parameters.AddWithValue("@nome",p.Nome);
+            cmd.Parameters.AddWithValue("@dataNascimento", p.DataNasc);
+            cmd.ExecuteNonQuery();
+
+            FecharConexao(); //desconectando
+
+        }
     }
 }
