e1149077ba88c398dbf75c88a5135b9131d23c07
Vinicius Fernando <31803742+V1n1f3rnando@users.noreply.github.com>
2018-07-25 06:34:41 -0300

    Nova entidade de criptografia.
    Criação da entidade CriptofrafiaSHA1.

---
+++ Projeto05/Projeto.Entidades/Seguranca/CriptografiaSHA1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Projeto.Entidades.Contratos;
+using System.Security.Cryptography;
+
+namespace Projeto.Entidades.Seguranca
+{
+    public class CriptografiaSHA1 : ICriptografia
+    {
+        public string Encriptar(string valor)
+        {
+            SHA1 sha1 = new SHA1CryptoServiceProvider();
+
+            //convertendo o valor de string para bytes
+            byte[] valorEmBytes = Encoding.UTF8.GetBytes(valor);
+
+            //realizar criptografia 
+            byte[] hash = sha1.ComputeHash(valorEmBytes);
+
+            //Convertendo o resultado da criptografia (hash)
+            // de byte para string 
+
+            string resultado = string.Empty;
+
+            //varrendo o vetor de bytes
+            foreach (var b in hash)
+            {
+                resultado += b.ToString("X2"); // X -> hexadecimal
+            }
+
+            //retornando
+            return resultado;
+        }
+    }
+}
