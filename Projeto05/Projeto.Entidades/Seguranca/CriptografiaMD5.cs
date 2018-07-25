25a94dde0f3e33bb14392d5da5e6a37fc9b7ff22
Vinicius Fernando <31803742+V1n1f3rnando@users.noreply.github.com>
2018-07-25 06:25:33 -0300

    Finalizanod a entidades CriptografiaMD5

--- Projeto05/Projeto.Entidades/Seguranca/CriptografiaMD5.cs
+++ Projeto05/Projeto.Entidades/Seguranca/CriptografiaMD5.cs
@@ -3,10 +3,36 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Projeto.Entidades.Contratos;
+using System.Security.Cryptography; //importando 
 
 namespace Projeto.Entidades.Seguranca
 {
-    class CriptografiaMD5
+    public class CriptografiaMD5 : ICriptografia
     {
+        public string Encriptar(string valor)
+        {
+            MD5 md5 = new MD5CryptoServiceProvider();
+
+            //Convertendo o valor de string para bytes
+            byte[] valorEmBytes = Encoding.UTF8.GetBytes(valor);
+
+            //realizar a criptografia
+            byte[] hash = md5.ComputeHash(valorEmBytes);
+
+            //Convertendo o resultado da criptografia (hash)
+            // de formato byte para formato string
+
+            string resultado = string.Empty;
+
+            //varrendo vetor de bytes
+            foreach (var b in hash)
+            {
+                resultado += b.ToString("X2"); // X -> hexadecimal 
+            }
+
+            //retonando resultado 
+            return resultado;
+        }
     }
 }
