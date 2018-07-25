e83411cd6d3c808ffd861077f4c9288ee8576499
Vinicius Fernando <31803742+V1n1f3rnando@users.noreply.github.com>
2018-07-25 06:15:57 -0300

    Início do projeto criptografia md5

---
+++ Projeto05/Projeto.Entidades/Contratos/ICriptografia.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projeto.Entidades.Contratos
+{
+    public interface ICriptografia
+    {
+        //Método abstrato
+        string Encriptar(string valor);
+    }
+}
---
+++ Projeto05/Projeto.Entidades/Usuario.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projeto.Entidades
+{
+    public class Usuario
+    {
+        //Encapsulamento inplícito 
+        public Guid IdUsuario { get; set; }
+        public string Login { get; set; }
+        public string Senha { get; set; }
+
+        //Construtor
+        public Usuario()
+        {
+
+        }
+
+        //Sobrecarga de construtor 
+        public Usuario(Guid idUsuario, string login, string senha)
+        {
+            IdUsuario = idUsuario;
+            Login = login;
+            Senha = senha;
+        }
+
+        //Sobrescrita do método ToString da classe object
+        public override string ToString()
+        {
+            return string.Format("ID:{0}, Login:{1}, Senha:{2}", IdUsuario, Login, Senha);
+        }
+    }
+}
